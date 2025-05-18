283e9063df2a91c5abf08add604e69ee5f4f373e
Bita Yasaei <bitays2002@gmail.com>
2025-05-18 14:43:05 +0000

    Create and style h2 element to show player's turn

--- game.js
+++ game.js
@@ -2,3 +2,8 @@ const board = document.getElementById('board')
 const squares = document.getElementsByClassName('square')
 const players = ['X', 'O']
 let currentPlayer = players[0]
+const endMessage = document.createElement('h2')
+endMessage.textContent = `X's turn!`
+endMessage.style.marginTop = '30px'
+endMessage.style.textAlign='center'
+board.after(endMessage)
