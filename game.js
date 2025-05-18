42e5e13617a69c13115704e0069ae89601b52a22
Bita Yasaei <bitays2002@gmail.com>
2025-05-18 15:06:39 +0000

    Define winning combinations and checkWin function

--- game.js
+++ game.js
@@ -7,3 +7,22 @@ endMessage.textContent = `X's turn!`
 endMessage.style.marginTop = '30px'
 endMessage.style.textAlign='center'
 board.after(endMessage)
+const winning_combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+function checkWin(currentPlayer) {
+    for(let i = 0; i < winning_combinations.length; i++){
+        const [a, b, c] = winning_combinations[i]
+        if(squares[a].textContent === currentPlayer && squares[b].textContent === currentPlayer && squares[c].textContent === currentPlayer){
+            return true
+        }
+    }
+    return false
+}
